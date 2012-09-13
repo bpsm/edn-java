8c0122224023bac95643aa1ecb4d6c805befed33
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2012-09-13 22:18:31 +0200

    put ugly date parsing machinery into InstantUtils for better testability
    All the hairy logic for parsing instants and creating Date (or
    Calendar or Timestamp) objects from the result is now in static
    methods in InstantUtils. This makes them easier to reuse and to test.
    
    Testing revealed just how hacked-up and broken my earlier attempt to
    provide #inst handler was. (Would it be a good time to mention that
    Java's native date/time APIs are a disgrace?)
    
    InstantUtils parses RFC3339 strings to ParsedInstant values. If you
    wish to provide a custom Handler for #inst, extend
    AbstractInstantHandler and implement translate(ParsedInstant).

--- src/main/java/bpsm/edn/parser/handlers/AbstractInstantHandler.java
+++ src/main/java/bpsm/edn/parser/handlers/AbstractInstantHandler.java
@@ -1,70 +1,19 @@
 // (c) 2012 B Smith-Mannschott -- Distributed under the Eclipse Public License
 package bpsm.edn.parser.handlers;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import bpsm.edn.model.Tag;
 import bpsm.edn.parser.EdnException;
 import bpsm.edn.parser.TagHandler;
 
 public abstract class AbstractInstantHandler implements TagHandler {
 
-    private static final Pattern INSTANT = Pattern.compile(
-            "(\\d\\d\\d\\d)(?:-(\\d\\d)(?:-(\\d\\d)(?:[T](\\d\\d)(?::(\\d\\d)"+
-            "(?::(\\d\\d)(?:[.](\\d+))?)?)?)?)?)?"+
-            "(?:[Z]|([-+])(\\d\\d):(\\d\\d))?");
-
-    protected abstract Object transform(
-            int years, int months, int days, 
-            int hours, int minutes, int seconds, int nanoseconds, 
-            int offsetSign, int offsetHours, int offsetMinutes);
-    
     public Object transform(Tag tag, Object value) {
         if (!(value instanceof String)) {
             throw new EdnException(tag.toString() + " expects a String.");
         }
-        Matcher m = INSTANT.matcher((String)value);
-        if (!m.matches()) {
-            throw new EdnException("Can't parse " + tag.toString() + "\"" + value + "\"");
-        }
-        
-        int years = parseIntOrElse(m.group(1), Integer.MIN_VALUE);
-        int months = parseIntOrElse(m.group(2), 1);
-        int days = parseIntOrElse(m.group(3), 1);
-        int hours = parseIntOrElse(m.group(4), 0);
-        int minutes = parseIntOrElse(m.group(5), 0);
-        int seconds = parseIntOrElse(m.group(6), 0);
-        int nanoseconds;
-        if (m.group(7) == null) {
-            nanoseconds = 0;
-        } else {
-            String s = m.group(7);
-            s = s + "000000000".substring(s.length());
-            nanoseconds = Integer.parseInt(s);
-        }
-        int offsetSign;
-        if (m.group(8) == null) {
-            offsetSign = 0;
-        } else {
-            offsetSign = "-".equals(m.group(8)) ? -1 : 1;
-        }
-        int offsetHours = parseIntOrElse(m.group(9), 0);
-        int offsetMinutes = parseIntOrElse(m.group(10), 0);
+        return transform(InstantUtils.parse((String)value));
+    }
 
-        return transform(
-                years, months, days, hours, minutes, seconds, nanoseconds, 
-                offsetSign, offsetHours, offsetMinutes);
-    }
-    
-    private int parseIntOrElse(String s, int alternative) {
-        if (s == null) {
-            return alternative;
-        }
-        return Integer.parseInt(s);
-    }
+    protected abstract Object transform(ParsedInstant pi);
 
 }
