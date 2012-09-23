e3c9af048c37c881ef138004f9c014f39bf46d20
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2012-09-23 19:35:57 +0200

    issue 4: remove Keyword.equals() and Keyword.hashCode()
    Since Keywords are interned globally reference equality (as provided
    by Object.equals() and Object.hashCode()) is equivalent to value
    equality. We no longer need to override equals() and hashCode().

--- src/main/java/bpsm/edn/model/Keyword.java
+++ src/main/java/bpsm/edn/model/Keyword.java
@@ -1,9 +1,6 @@
 // (c) 2012 B Smith-Mannschott -- Distributed under the Eclipse Public License
 package bpsm.edn.model;
 
-import java.lang.ref.Reference;
-import java.util.concurrent.ConcurrentHashMap;
-
 import bpsm.edn.parser.util.Interner;
 
 public final class Keyword implements Named, Comparable<Keyword> {
@@ -30,36 +27,6 @@ public final class Keyword implements Named, Comparable<Keyword> {
 
     public String toString() {
         return ":" + sym.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((sym == null) ? 0 : sym.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Keyword other = (Keyword) obj;
-        if (sym == null) {
-            if (other.sym != null) {
-                return false;
-            }
-        } else if (!sym.equals(other.sym)) {
-            return false;
-        }
-        return true;
     }
 
     public int compareTo(Keyword o) {
