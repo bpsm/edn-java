584be7aa943ce7bd2ed3daca7bd47910f3982236
mikera <mike.r.anderson.13@gmail.com>
2012-10-03 11:06:46 +0800

    Added specialised StringPrinter class for writing edn to Strings

---
+++ src/test/java/bpsm/edn/printer/StringPrinterTest.java
@@ -0,0 +1,22 @@
+package bpsm.edn.printer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class StringPrinterTest {
+
+	@Test
+	public void TestBasicStringPrinter() {
+		StringPrinter sp=new StringPrinter();
+		
+	   	ArrayList<Object> al=new ArrayList<Object>();
+    	al.add(1);
+    	al.add(2);
+    	sp.printValue(al);
+    	assertEquals("[1 2]", sp.toString());	
+    	sp.close();
+    }
+}
