c2bbffe29c1f8247b59a12453d5847e22d53eb6b
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2012-10-21 15:13:36 +0200

    make Token public, and document it

--- src/main/java/us/bpsm/edn/parser/Token.java
+++ src/main/java/us/bpsm/edn/parser/Token.java
@@ -1,15 +1,42 @@
 // (c) 2012 B Smith-Mannschott -- Distributed under the Eclipse Public License
 package us.bpsm.edn.parser;
 
-enum Token {
+/**
+ * This enum represents a subset of the elements that can occur in a
+ * {@link TokenSeq}.
+ */
+public enum Token {
+
+    /** Signals the end of input. */
     END_OF_INPUT,
+
+    /** A '(', which begins a list */
     BEGIN_LIST,
+
+    /** A ')', which ends a list. */
     END_LIST,
+
+    /** A '[', which begins a vector */
     BEGIN_VECTOR,
+
+    /** A ']', which ends a vector */
     END_VECTOR,
+
+    /** A '#{', which begins a set */
     BEGIN_SET,
+
+    /** A '{', which begins a map */
     BEGIN_MAP,
+
+    /** A '}', which ends a set or map */
     END_MAP_OR_SET,
+
+    /** A 'nil', which a parser should interpret as a Java null. */
     NIL,
+
+    /**
+     * A '#_', which instructs the parser to ignore the next value parsed from
+     * the input.
+     */
     DISCARD;
 }
