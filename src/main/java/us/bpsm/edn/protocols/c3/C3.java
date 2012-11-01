1c3e123de15c577726ae18438b17eeec87bb4961
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2012-11-01 20:45:39 +0100

    rewrite C3 to be shorter and clearer
    This is one of those algorithms that can be more succinctly expressed
    in a language with a functional flavor, but we're stuck with plain
    Java here, so imperative, it is.

--- src/main/java/us/bpsm/edn/protocols/c3/C3.java
+++ src/main/java/us/bpsm/edn/protocols/c3/C3.java
@@ -1,115 +1,90 @@
 package us.bpsm.edn.protocols.c3;
 
-import static us.bpsm.edn.protocols.c3.Cons.cons;
-import static us.bpsm.edn.protocols.c3.Cons.first;
-import static us.bpsm.edn.protocols.c3.Cons.rest;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 class C3 {
 
-    static int b2i(boolean b) {
-        return b ? 1 : 0;
+    static List<Class<?>> methodResolutionOrder(Class<?> c) {
+        List<List<Class<?>>> seqsToMerge = new ArrayList<List<Class<?>>>();
+        seqsToMerge.add(asList(c));
+        List<Class<?>> supers = supers(c);
+        for (Class<?> s: supers) {
+            seqsToMerge.add(methodResolutionOrder(s));
+        }
+        seqsToMerge.add(supers);
+        return merge(seqsToMerge);
     }
 
-    static Cons<Class<?>> supers(Class<?> c) {
+    private static List<Class<?>> asList(Class<?> c) {
+        List<Class<?>> result = new ArrayList<Class<?>>(1);
+        result.add(c);
+        return result;
+    }
+
+    private static List<Class<?>> supers(Class<?> c) {
         Class<?> sc = c.getSuperclass();
         Class<?>[] interfaces = c.getInterfaces();
-        Cons<Class<?>> result = null;
-        for (int i = interfaces.length - 1; i >= 0; i--) {
-            result = cons(interfaces[i], result);
-        }
+        List<Class<?>> result = new ArrayList<Class<?>>();
         if (sc != null) {
-            result = cons(sc, result);
+            result.add(sc);
+        }
+        for (Class<?> i: interfaces) {
+            result.add(i);
         }
         return result;
     }
 
-    static List<Class<?>> methodResolutionOrder(Class<?> c) {
-        return Cons.toList(mro(c));
+    private static List<Class<?>> merge(List<List<Class<?>>> seqsToMerge) {
+        List<Class<?>> result = new ArrayList<Class<?>>();
+        while (!allAreEmpty(seqsToMerge)) {
+            Class<?> candidate = findCandidate(seqsToMerge);
+            if (candidate == null) {
+                throw new RuntimeException("Inconsistent hierarchy");
+            }
+            result.add(candidate);
+            removeCandidate(seqsToMerge, candidate);
+        }
+        return result;
     }
 
-    static Cons<Class<?>> mro(Class<?> c) {
-        Cons<Class<?>> supers = supers(c);
-        List<Cons<Class<?>>> supermros = new ArrayList<Cons<Class<?>>>();
-        for (Cons<Class<?>> ss = supers; ss != null; ss = rest(ss)) {
-            supermros.add(mro(first(ss)));
-        }
-        return cons(c, merge(supermros));
-    }
-
-    private static Cons<Class<?>> merge(List<Cons<Class<?>>> supermros) {
-        List<Class<?>> merged = new ArrayList<Class<?>>();
-        while (true) {
-            List<Class<?>> heads = heads(supermros);
-            if (heads.size() == 0) {
-                return Cons.toCons(merged);
-            }
-            List<Cons<Class<?>>> tails = tails(supermros);
-            Class<?> h = findGoodHead(heads, tails);
-            if (h != null) {
-                removeMatchingHeadFromAll(h, supermros);
-                merged.add(h);
-            } else {
-                throw new RuntimeException("Inconsistently ordered hierarchy");
-            }
-        }
-    }
-
-    private static void removeMatchingHeadFromAll(Class<?> h,
-            List<Cons<Class<?>>> supermros) {
-        for (int i = 0; i < supermros.size(); i++) {
-            if (Cons.first(supermros.get(i)) == h) {
-                supermros.set(i, Cons.rest(supermros.get(i)));
-            }
-        }
-        for(Iterator<Cons<Class<?>>> it = supermros.iterator(); it.hasNext();) {
-            if (it.next() == null) {
-                it.remove();
-            }
-        }
-    }
-
-    private static Class<?> findGoodHead(List<Class<?>> heads,
-            List<Cons<Class<?>>> tails) {
-        for (Class<?> h: heads) {
-            if (headIsGood(h, tails)) {
-                return h;
-            }
-        }
-        return null;
-    }
-
-
-    private static boolean headIsGood(Class<?> h, List<Cons<Class<?>>> tails) {
-        for (Cons<Class<?>> t: tails) {
-            if (Cons.contains(t, h)) {
+    private static boolean allAreEmpty(List<List<Class<?>>> lists) {
+        for (List<?> l: lists) {
+            if (!l.isEmpty()) {
                 return false;
             }
         }
         return true;
     }
 
-    private static List<Cons<Class<?>>> tails(List<Cons<Class<?>>> supermros) {
-        List<Cons<Class<?>>> tails = new ArrayList<Cons<Class<?>>>();
-        for (Cons<Class<?>> c: supermros) {
-            if (c != null && c.rest != null) {
-                tails.add(c.rest);
+    private static Class<?> findCandidate(List<List<Class<?>>> seqsToMerge) {
+        for (List<Class<?>> seq: seqsToMerge) {
+            if (!seq.isEmpty() && !occursInSomeTail(seqsToMerge, seq.get(0))) {
+                return seq.get(0);
             }
         }
-        return tails;
+        return null;
     }
 
-    private static List<Class<?>> heads(List<Cons<Class<?>>> supermros) {
-        List<Class<?>> heads = new ArrayList<Class<?>>();
-        for (Cons<Class<?>> c: supermros) {
-            if (c != null) {
-                heads.add(c.first);
+    private static boolean occursInSomeTail(List<List<Class<?>>> seqsToMerge,
+        Object c) {
+        for (List<?> seq: seqsToMerge) {
+            for (int i = 1; i < seq.size(); i++) {
+                if (c.equals(seq.get(i))) {
+                    return true;
+                }
             }
         }
-        return heads;
+        return false;
+    }
+
+    private static void removeCandidate(List<List<Class<?>>> seqsToMerge,
+        Class<?> candidate) {
+        for (List<Class<?>> seq: seqsToMerge) {
+            if (!seq.isEmpty() && candidate.equals(seq.get(0))) {
+                seq.remove(0);
+            }
+        }
     }
 
 }
