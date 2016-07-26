64d33cbe41be993082abe4e5a3903d96655c163a
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2016-07-26 21:01:24 +0200

    issue46: make PrintingExamples insenstive to hash ordering
    The two printing examples assume an ordering for the keys in the example
    map. This ordering can change between Java versions or, indeed, any time
    it feels like it.

--- src/test/java/us/bpsm/edn/examples/PrintingExamples.java
+++ src/test/java/us/bpsm/edn/examples/PrintingExamples.java
@@ -1,26 +1,30 @@
 package us.bpsm.edn.examples;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 import us.bpsm.edn.parser.Parser;
 import us.bpsm.edn.parser.Parsers;
 import us.bpsm.edn.printer.Printers;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PrintingExamples {
     @Test
     public void printCompactly() {
-        Assert.assertEquals(
-                EXPECTED_COMPACT_RENDERING,
-                Printers.printString(
-                        Printers.defaultPrinterProtocol(), VALUE_TO_PRINT));
+        Assert.assertThat(ACCEPTABLE_COMPACT_RENDERINGS,
+                CoreMatchers.hasItem(Printers.printString(
+                        Printers.defaultPrinterProtocol(),
+                        VALUE_TO_PRINT)));
     }
 
     @Test
     public void printPretty() {
-        Assert.assertEquals(
-                EXPECTED_PRETTY_RENDERING,
-                Printers.printString(
-                        Printers.prettyPrinterProtocol(), VALUE_TO_PRINT));
+        Assert.assertThat(ACCEPTABLE_PRETTY_RENDERINGS,
+                CoreMatchers.hasItem(Printers.printString(
+                        Printers.prettyPrinterProtocol(),
+                        VALUE_TO_PRINT)));
     }
 
     static final Object VALUE_TO_PRINT;
@@ -31,10 +35,12 @@ public class PrintingExamples {
                 " [x/y] 3.14159}\n"));
     }
 
-    static final String EXPECTED_COMPACT_RENDERING =
-            "{:a[1 2 3][x/y]3.14159}";
+    static final List<String> ACCEPTABLE_COMPACT_RENDERINGS = Arrays.asList(
+            "{:a[1 2 3][x/y]3.14159}",
+            "{[x/y]3.14159 :a[1 2 3]}"
+    );
 
-    static final String EXPECTED_PRETTY_RENDERING =
+    static final List<String> ACCEPTABLE_PRETTY_RENDERINGS = Arrays.asList(
             "{"           + "\n" +
             "  :a ["      + "\n" +
             "    1"       + "\n" +
@@ -44,5 +50,16 @@ public class PrintingExamples {
             "  ["         + "\n" +
             "    x/y"     + "\n" +
             "  ] 3.14159" + "\n" +
-            "}";
+            "}",
+            "{"           + "\n" +
+            "  ["         + "\n" +
+            "    x/y"     + "\n" +
+            "  ] 3.14159" + "\n" +
+            "  :a ["      + "\n" +
+            "    1"       + "\n" +
+            "    2"       + "\n" +
+            "    3"       + "\n" +
+            "  ]"         + "\n" +
+            "}"
+    );
 }
