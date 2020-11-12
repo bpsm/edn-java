e25b62ee7513c25826565e67b392b794f75d008d
Wang <chw6@fa20-cs527-063.cs.illinois.edu>
2020-11-12 13:59:03 -0600

    Fix flaky test by using LinkedHashMap and LinkedHashSet

--- src/test/java/us/bpsm/edn/printer/PrinterTest.java
+++ src/test/java/us/bpsm/edn/printer/PrinterTest.java
@@ -109,10 +109,10 @@ public class PrinterTest {
 
     @Test
     public void testPrettyPrinting() {
-        Map<Integer, String> m = new HashMap();
+        Map<Integer, String> m = new LinkedHashMap();
         m.put(3, "Three");
         m.put(4, "Four");
-        List<?> list = Arrays.asList(new HashSet(Arrays.asList(1, 2)), m);
+        List<?> list = Arrays.asList(new LinkedHashSet(Arrays.asList(1, 2)), m);
         String s = Printers.printString(Printers.prettyPrinterProtocol(), list);
         assertEquals("[\n  #{\n    1\n    2\n  }\n  {\n    3 \"Three\"\n    4 \"Four\"\n  }\n]", s);
     }
